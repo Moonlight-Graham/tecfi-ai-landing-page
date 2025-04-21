00fca91a63037f9072a927278314f6d0a5a029c6
Moonlight-Graham <ryanputz81@gmail.com>
2025-04-20 19:53:43 -0500

    updated icon

--- public/brainzyai-icon-32x32.png.jsx
+++ public/brainzyai-icon-32x32.png.jsx
@@ -1,3 +1,16 @@
-<a href="/brainzyai-icon-32x32.png" download="brainzy-icon.png">
-  Download Icon
+<a
+  href="/brainzyai-icon-32x32.png"
+  download="brainzy-icon.png"
+  style={{
+    padding: '8px 16px',
+    backgroundColor: '#3b82f6',
+    color: '#fff',
+    borderRadius: '6px',
+    textDecoration: 'none',
+    display: 'inline-block',
+    marginTop: '10px'
+  }}
+>
+  ⬇️ Download BRANI Icon
 </a>
+
